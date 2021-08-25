a9a4e1900f72a4da84f8f46939a49e6d2e7c0e81
Ferks <69549678+Ferks-FK@users.noreply.github.com>
2021-08-25 12:52:48 -0400

    Update build.sh

--- themes/version1.x/UltraCheese/build.sh
+++ themes/version1.x/UltraCheese/build.sh
@@ -13,7 +13,7 @@ set -e
 ########################################################
 
 #### Variables ####
-SCRIPT_VERSION="v0.1"
+SCRIPT_VERSION="v0.2"
 SUPPORT_LINK="https://discord.gg/buDBbSGJmQ"
 
 
