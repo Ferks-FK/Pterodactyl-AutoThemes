f7593de5f2224e370295d1aa225353e509ae4443
Ferks-FK <fernandokaiquecnp2014@gmail.com>
2021-08-26 17:16:39 -0400

    Theme for 0.7.19

---
+++ install.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+
+set -e
+
+########################################################
+# 
+#         Pterodactyl-AutoThemes Installation
+#
+#         Created and maintained by Ferks-FK
+#
+#            Protected by GPL 3.0 License
+#
+########################################################
+
+SCRIPT_VERSION="v0.4"
+
+
+print_brake() {
+  for ((n = 0; n < $1; n++)); do
+    echo -n "#"
+  done
+  echo ""
+}
+
+hyperlink() {
+  echo -e "\e]8;;${1}\a${1}\e]8;;\a"
+}
+
+#### Colors ####
+
+GREEN="\e[0;92m"
+YELLOW="\033[1;33m"
+reset="\e[0m"
+red='\033[0;31m'
+
+error() {
+  COLOR_RED='\033[0;31m'
+  COLOR_NC='\033[0m'
+
+  echo ""
+  echo -e "* ${COLOR_RED}ERROR${COLOR_NC}: $1"
+  echo ""
+}
+
+
+#### Check Sudo ####
+
+if [[ $EUID -ne 0 ]]; then
+  echo "* This script must be executed with root privileges (sudo)." 1>&2
+  exit 1
+fi
+
+
+#### Check Curl ####
+
+if ! [ -x "$(command -v curl)" ]; then
+  echo "* curl is required in order for this script to work."
+  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
+  exit 1
+fi
+
+cancel() {
+echo
+echo -e "* ${red}Installation Canceled!${reset}"
+done=true
+exit 1
+}
+
+done=false
+
+echo
+print_brake 70
+echo "* Pterodactyl-AutoThemes Script @ $SCRIPT_VERSION"
+echo
+echo "* Copyright (C) 2021 - 2021, Ferks-FK."
+echo "* https://github.com/Ferks-FK/Pterodactyl-AutoThemes"
+echo
+echo "* This script is not associated with the official Pterodactyl Project."
+print_brake 70
+echo
+
+Default() {
+bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/themes/version1.x/Default/build.sh)
+}
+
+Dracula() {
+bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/themes/version1.x/Dracula/build.sh)
+}
+
+Enola() {
+bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/themes/version1.x/Enola/build.sh)
+}
+
+Twilight() {
+bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/themes/version1.x/Twilight/build.sh)
+}
+
+BlackEndSpace() {
+bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/themes/version0.7.19/BlackEndSpace/build.sh)
+}
+
+BlueBrick() {
+bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/themes/version0.7.19/BlueBrick/build.sh)
+}
+
+LimeStitch() {
+bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/themes/version0.7.19/LimeStitch/build.sh)
+}
+
+MinecraftMadness() {
+bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/themes/version0.7.19/MinecraftMadness/build.sh)
+}
+
+NothingButGraphite() {
+bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/themes/version0.7.19/NothingButGraphite/build.sh)
+}
+
+RedApe() {
+bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/themes/version0.7.19/RedApe/build.sh)
+}
+
+TangoTwist() {
+bash <(curl -s https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/themes/version0.7.19/TangoTwist/build.sh)
+}
+
+
+while [ "$done" == false ]; do
+  options=(
+    "Back to default theme (Only 1.x)"
+    "Install Dracula (Only 1.x)"
+    "Install Enola (Only 1.x)"
+    "Install Twilight (Only 1.x)"
+    "Install Black End Space (Only 0.7.19)"
+    "Install Blue Brick (Only 0.7.19)"
+    "Install Lime Stitch (Only 0.7.19)"
+    "Install Minecraft Madness (Only 0.7.19)"
+    "Install Nothing But Graphite (Only 0.7.19)"
+    "Install Red Ape (Only 0.7.19)"
+    "Install Tango Twist (Only 0.7.19)"
+    
+    
+    "Cancel Installation"
+  )
+  
+  actions=(
+    "Default"
+    "Dracula"
+    "Enola"
+    "Twilight"
+    "BlackEndSpace"
+    "BlueBrick"
+    "LimeStitch"
+    "MinecraftMadness"
+    "NothingButGraphite"
+    "RedApe"
+    "TangoTwist"
+    
+    
+    "cancel"
+  )
+  
+  echo "* Which theme do you want to install?"
+  echo
+  
+  for i in "${!options[@]}"; do
+    echo "[$i] ${options[$i]}"
+  done
+  
+  echo
+  echo -n "* Input 0-$((${#actions[@]} - 1)): "
+  read -r action
+  
+  [ -z "$action" ] && error "Input is required" && continue
+  
+  valid_input=("$(for ((i = 0; i <= ${#actions[@]} - 1; i += 1)); do echo "${i}"; done)")
+  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Invalid option"
+  [[ " ${valid_input[*]} " =~ ${action} ]] && done=true && eval "${actions[$action]}"
+done
---
+++ themes/version1.x/Twilight/build.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+
+set -e
+
+########################################################
+# 
+#         Pterodactyl-AutoThemes Installation
+#
+#         Created and maintained by Ferks-FK
+#
+#            Protected by GPL 3.0 License
+#
+########################################################
+
+#### Variables ####
+SCRIPT_VERSION="v0.4"
+TWILIGHT="https://pterothemes.gq/latest/Twilight/admin.css"
+SUPPORT_LINK="https://discord.gg/buDBbSGJmQ"
+
+
+print_brake() {
+  for ((n = 0; n < $1; n++)); do
+    echo -n "#"
+  done
+  echo ""
+}
+
+
+hyperlink() {
+  echo -e "\e]8;;${1}\a${1}\e]8;;\a"
+}
+
+
+#### Colors ####
+
+GREEN="\e[0;92m"
+YELLOW="\033[1;33m"
+reset="\e[0m"
+
+
+#### OS check ####
+
+check_distro() {
+  if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$(echo "$ID")
+    OS_VER=$VERSION_ID
+  elif type lsb_release >/dev/null 2>&1; then
+    OS=$(lsb_release -si)
+    OS_VER=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    OS=$(echo "$DISTRIB_ID")
+    OS_VER=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then
+    OS="debian"
+    OS_VER=$(cat /etc/debian_version)
+  elif [ -f /etc/SuSe-release ]; then
+    OS="SuSE"
+    OS_VER="?"
+  elif [ -f /etc/redhat-release ]; then
+    OS="Red Hat/CentOS"
+    OS_VER="?"
+  else
+    OS=$(uname -s)
+    OS_VER=$(uname -r)
+  fi
+
+  OS=$(echo "$OS")
+  OS_VER_MAJOR=$(echo "$OS_VER" | cut -d. -f1)
+}
+
+
+#### Install Dependencies ####
+
+dependencies() {
+echo
+print_brake 30
+echo -e "* ${GREEN}Installing dependencies...${reset}"
+print_brake 30
+echo
+case "$OS" in
+debian | ubuntu)
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - && apt-get install -y nodejs
+;;
+
+centos)
+[ "$OS_VER_MAJOR" == "7" ] && curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash - && sudo yum install -y nodejs yarn
+[ "$OS_VER_MAJOR" == "8" ] && curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash - && sudo dnf install -y nodejs yarn
+;;
+esac
+}
+
+
+#### Donwload Files ####
+print_brake 25
+echo -e "* ${GREEN}Downloading files...${reset}"
+print_brake 25
+download_files() {
+cd /var/www/pterodactyl/resources/scripts
+curl -o user.css https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/themes/version1.x/Twilight/user.css
+rm -R index.tsx
+curl -o index.tsx https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/themes/version1.x/Twilight/index.tsx
+cd
+cd /var/www/pterodactyl/resources/views/layouts
+rm -R admin.blade.php
+curl -o admin.blade.php https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/themes/version1.x/Twilight/admin.blade.php
+sed -i -e "s@<STYLE-ADMIN>@${TWILIGHT}@g" /var/www/pterodactyl/resources/views/layouts/admin.blade.php
+}
+
+#### Panel Production ####
+
+production() {
+DIR=/var/www/pterodactyl
+
+if [ -d "$DIR" ]; then
+echo
+print_brake 25
+echo -e "* ${GREEN}Producing panel...${reset}"
+print_brake 25
+npm i -g yarn
+cd /var/www/pterodactyl
+yarn install
+yarn add @emotion/react
+yarn build:production
+fi
+}
+
+
+bye() {
+print_brake 50
+echo
+echo -e "* ${GREEN}The theme ${YELLOW}Twilight${GREEN} was successfully installed.${reset}"
+echo -e "* ${GREEN}Thank you for using this script.${reset}"
+echo -e "* ${GREEN}Support group: $(hyperlink "$SUPPORT_LINK")${reset}"
+echo
+print_brake 50
+}
+
+
+#### Exec Script ####
+check_distro
+dependencies
+download_files
+production
+bye
