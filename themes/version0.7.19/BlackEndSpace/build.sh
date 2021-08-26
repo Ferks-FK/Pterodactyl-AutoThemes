181226604a77416aa639ffee84d556454d77ee83
Ferks <69549678+Ferks-FK@users.noreply.github.com>
2021-08-26 17:36:54 -0400

    Update build.sh

--- themes/version0.7.19/BlackEndSpace/build.sh
+++ themes/version0.7.19/BlackEndSpace/build.sh
@@ -113,7 +113,7 @@ print_brake 25
 cd /var/www/pterodactyl
 mkdir -p temp
 cd temp
-curl -sSLo BlackEndSpace.tar.gz https://raw.githubusercontent.com/Alok-NetWork/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/themes/version0.7.19/BlackEndSpace/BlackEndSpace.tar.gz
+curl -sSLo BlackEndSpace.tar.gz https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/themes/version0.7.19/BlackEndSpace/BlackEndSpace.tar.gz
 tar -xzvf BlackEndSpace.tar.gz
 cd BlackEndSpace
 cp -rf -- * /var/www/pterodactyl
