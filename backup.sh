8fc0a4bac16308d50ae85a422f73ebd9f35806ab
Ferks-FK <fernandokaiquecnp2014@gmail.com>
2022-03-13 12:31:07 -0400

    Fix error

--- backup.sh
+++ backup.sh
@@ -27,7 +27,6 @@ if [ -f "${INFORMATIONS}/background.txt" ]; then
 fi
 }
 
-
 print_brake() {
   for ((n = 0; n < $1; n++)); do
     echo -n "#"
@@ -35,27 +34,38 @@ print_brake() {
   echo ""
 }
 
+print() {
+  echo ""
+  echo -e "* ${GREEN}$1${RESET}"
+  echo ""
+}
+
+print_warning() {
+  echo ""
+  echo -e "* ${YELLOW}WARNING${RESET}: $1"
+  echo ""
+}
+
+print_error() {
+  echo ""
+  echo -e "* ${RED}ERROR${RESET}: $1"
+  echo ""
+}
 
 hyperlink() {
   echo -e "\e]8;;${1}\a${1}\e]8;;\a"
 }
 
-
-#### Colors ####
-
 GREEN="\e[0;92m"
 YELLOW="\033[1;33m"
-reset="\e[0m"
-red='\033[0;31m'
+RESET="\e[0m"
+RED='\033[0;31m'
 
 #### Find where pterodactyl is installed ####
 
 find_pterodactyl() {
-echo
-print_brake 47
-echo -e "* ${GREEN}Looking for your pterodactyl installation...${reset}"
-print_brake 47
-echo
+print "Looking for your pterodactyl installation..."
+
 sleep 2
 if [ -d "/var/www/pterodactyl" ]; then
     PTERO_INSTALL=true
@@ -73,85 +83,81 @@ fi
 update_variables
 }
 
-
 #### Deletes all files installed by the script ####
 
 delete_files() {
 #### THEMES DRACULA, ENOLA AND TWILIGHT ####
 if [ -f "$DET" ]; then
-  rm -r "$DET"
-  rm -r "$PTERO/resources/scritps/user.css"
+  rm -rf "$DET"
+  rm -rf "$PTERO/resources/scripts/user.css"
 fi
 #### THEMES DRACULA, ENOLA AND TWILIGHT ####
 
 #### THEME ZINGTHEME ####
 if [ -f "$ZING" ]; then
-  rm -r "$ZING"
-  rm -r "$PTERO/resources/scripts/components/server/files/FileViewer.tsx"
+  rm -rf "$ZING"
+  rm -rf "$PTERO/resources/scripts/components/server/files/FileViewer.tsx"
 fi
 #### THEME ZINGTHEME ####
 
 #### BACKGROUND VIDEO ####
 if [ -f "$PTERO/public/$BACKGROUND" ]; then
   cd "$PTERO/public" && rm -r "$BACKGROUND"
-  rm -r "$PTERO/resources/scripts/user.css"
-  rm -r "$INFORMATIONS"
+  rm -rf "$PTERO/resources/scripts/user.css"
+  rm -rf "$INFORMATIONS"
 fi
 #### BACKGROUND VIDEO ####
 }
 
-
 #### Restore Backup ####
 
 restore() {
-echo
-print_brake 35
-echo -e "* ${GREEN}Checking for a backup...${reset}"
-print_brake 35
-echo
+print "Checking for a backup..."
+
 if [ -d "$PTERO/PanelBackup[Auto-Themes]" ]; then
     cd "$PTERO/PanelBackup[Auto-Themes]"
     tar -xzvf "PanelBackup[Auto-Themes].tar.gz"
     rm -R "PanelBackup[Auto-Themes].tar.gz"
     cp -r -- * .env "$PTERO"
-    rm -r "$PTERO/PanelBackup[Auto-Themes]"
+    rm -rf "$PTERO/PanelBackup[Auto-Themes]"
   else
-    print_brake 45
-    echo -e "* ${red}There was no backup to restore, Aborting...${reset}"
-    print_brake 45
-    echo
+    print_error "There was no backup to restore, Aborting..."
     exit 1
 fi
 }
-
 
 bye() {
 print_brake 50
 echo
 echo -e "${GREEN}* Backup restored successfully!"
 echo -e "* Thank you for using this script."
-echo -e "* Support group: ${YELLOW}$(hyperlink "$SUPPORT_LINK")${reset}"
+echo -e "* Support group: ${YELLOW}$(hyperlink "$SUPPORT_LINK")${RESET}"
 echo
 print_brake 50
 }
 
-
 #### Exec Script ####
 find_pterodactyl
 if [ "$PTERO_INSTALL" == true ]; then
-    echo
-    print_brake 60
-    echo -e "* ${GREEN}Installation of the panel found, continuing the backup...${reset}"
-    print_brake 60
-    echo
+    print "Installation of the panel found, continuing the backup..."
     delete_files
     restore
     bye
   elif [ "$PTERO_INSTALL" == false ]; then
-    echo
-    print_brake 66
-    echo -e "* ${red}The installation of your panel could not be located, aborting...${reset}"
-    print_brake 66
-    echo
-    exit 1
+    print_warning "The installation of your panel could not be located."
+    echo -e "* ${GREEN}EXAMPLE${RESET}: ${YELLOW}/var/www/mypanel${RESET}"
+    echo -ne "* Enter the pterodactyl installation directory manually: "
+    read -r MANUAL_DIR
+    if [ -d "$MANUAL_DIR" ]; then
+        print "Directory has been found!"
+        PTERO="$MANUAL_DIR"
+        echo "$MANUAL_DIR" >> "$INFORMATIONS/custom_directory.txt"
+        update_variables
+        delete_files
+        restore
+        bye
+      else
+        print_error "The directory you entered does not exist."
+        find_pterodactyl
+    fi
 fi
